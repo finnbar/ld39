c9aa7aaa2f6ac2cea5ec12e2d4a1e8e0d29b1684
Finnbar Keating <finnjkeating@aol.com>
2017-07-29 18:09:52 +0100

    For debug. Still need to fix bug though.

--- blend_testing/main.lua
+++ blend_testing/main.lua
@@ -4,20 +4,30 @@ for _,j in pairs(requires) do
     require(j)
 end
 
+hide = true
+
 function love.load()
     math.randomseed(os.time())
     maze = makemaze()
 end
 
 function love.draw()
-    love.graphics.setBlendMode("alpha", "alphamultiply")
-    love.graphics.setColor(255,255,255)
     local x, y = love.mouse.getPosition()
-    love.graphics.circle("fill",x,y,50)
-    love.graphics.setBlendMode("darken", "premultiplied")
+    if hide then
+        love.graphics.setBlendMode("alpha", "alphamultiply")
+        love.graphics.setColor(255,255,255)
+        love.graphics.circle("fill",x,y,50)
+        love.graphics.setBlendMode("darken", "premultiplied")
+    end
     love.graphics.setColor(255, 255, 255, 100)
     drawmaze(maze)
-    love.graphics.setBlendMode("alpha", "alphamultiply")
-    love.graphics.setColor(250, 0, 0)
-    love.graphics.circle("line",x,y,50)
+    if hide then
+        love.graphics.setBlendMode("alpha", "alphamultiply")
+        love.graphics.setColor(250, 0, 0)
+        love.graphics.circle("line",x,y,50)
+    end
+end
+
+function love.keypressed(k)
+    hide = not hide
 end
